86fd451f27821a819c04c14ba5e8a4693621025e
Shallker <imshallker@gmail.com>
2014-12-04 15:59:36 +0800

    引入事件机制和添加setInputText方法

--- script/date-selector.js
+++ script/date-selector.js
@@ -1,4 +1,9 @@
 define('common-ui:date-selector', function (require, exports, module) {
+    var j = jQuery;
+    var inherit = require('util:inherit');
+    var EventTarget = require('event:EventTarget');
+
+
     /**
      * 导出
      */
@@ -14,6 +19,33 @@ define('common-ui:date-selector', function (require, exports, module) {
         var me = this;
 
         me.el = el;
-        console.log('el', el);
+
+        // 搜集自身元素
+        me.inputTextElement = j(me.el).find('.date-selector-input .input-text').get(0);
+    }
+
+
+    /**
+     * DateSelector继承自EventTarget
+     */
+    inherit(DateSelector, EventTarget);
+
+
+    /**
+     * 设置显示文字
+     * @param {String} text 文本
+     * @return {undefined}
+     */
+    DateSelector.prototype.setInputText = function (text) {
+        j(this.inputTextElement).text(text);
+    }
+
+
+    /**
+     * 获取显示文字
+     * @return {String} 文本
+     */
+    DateSelector.prototype.getInputText = function () {
+        return this.inputTextElement.innerText;
     }
 });
